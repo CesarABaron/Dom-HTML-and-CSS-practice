ce9ff3f6600bfafcc907e1b3ae1cc558687bed87
Cesar Baron <cesarbaronr@gmail.com>
2025-04-17 18:48:38 -0500

    css adjustments

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const images = [
-    `assets/champion1.png`,
-    `assets/champion2.png`,
-    `assets/champion3.png`,
-    `assets/champion4.png`,
-    `assets/champion5.png`,
-    `assets/champion6.png`,
-    `assets/champion7.png`,
-    `assets/champion8.png`,
-    `assets/champion9.png`,
-    `assets/champion10.png`,
-  ];
-  
+  `assets/champion1.png`,
+  `assets/champion2.png`,
+  `assets/champion3.png`,
+  `assets/champion4.png`,
+  `assets/champion5.png`,
+  `assets/champion6.png`,
+  `assets/champion7.png`,
+  `assets/champion8.png`,
+  `assets/champion9.png`,
+  `assets/champion10.png`,
+];
 
 const botonEnviar = document.getElementById("enviar");
-const botonCambiar = document.getElementById("cambiarImg")
-let counter = 0
+const botonCambiar = document.getElementById("cambiarImg");
+let counter = 0;
 
 const addname = () => {
   const p = document.getElementById("creador");
@@ -26,21 +25,18 @@ botonEnviar.addEventListener("click", addname);
 
 /////////////////////////////////////////////////////////////
 
-
 const countImages = () => {
+  const imagenLol = document.getElementById("imagenContainer");
 
-    const imagenLol = document.getElementById("imagenContainer")
+  if (counter === 0) {
+    counter++;
+  } else if (counter >= images.length - 1) {
+    counter = 0;
+  } else {
+    counter++;
+  }
 
-    if (counter === 0) {
-        counter++;
-    } else if (counter >= images.length-1) {
-        counter = 0;
-    } else {
-        counter++;
-    }
+  imagenLol.attributes.src.value = images[counter];
+};
 
-    console.log(imagenLol.attributes.src.value = images[counter])
-
-}
-
-botonCambiar.addEventListener("click", countImages)
+botonCambiar.addEventListener("click", countImages);
