7c1cf4dfad22d60096e02154dc5a48b5ed7f644a
Cesar Baron <cesarbaronr@gmail.com>
2025-04-16 23:06:45 -0500

    Dom practice visual almost complete

---
+++ app.js
@@ -0,0 +1,19 @@
+const boton = document.getElementById("enviar")
+
+
+boton.addEventListener("click",() => {
+    document.createElement("div");
+    document.classList.add("nuevoD");
+    console.log("sirve")})
+
+// const createDiv = () =>{
+
+//     document.createElement("div").classList.add("nuevoD")
+//     console.log("sirve")
+// }
+
+// const body = document.getElementsByName("body")[0]
+
+console.log(document.getElementsByName("body"))
+
+body.appendChild("click", createDiv())
