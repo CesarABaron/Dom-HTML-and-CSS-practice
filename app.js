f50e23b6237616bd45d5cf6e83925c6bb7771a0a
Cesar Baron <cesarbaronr@gmail.com>
2025-04-17 18:40:54 -0500

    Dom properties, java scrip logic, event listener, paths to images, labels of HTML and body properties

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-const boton = document.getElementById("enviar")
+const images = [
+    `assets/champion1.png`,
+    `assets/champion2.png`,
+    `assets/champion3.png`,
+    `assets/champion4.png`,
+    `assets/champion5.png`,
+    `assets/champion6.png`,
+    `assets/champion7.png`,
+    `assets/champion8.png`,
+    `assets/champion9.png`,
+    `assets/champion10.png`,
+  ];
+  
+
+const botonEnviar = document.getElementById("enviar");
+const botonCambiar = document.getElementById("cambiarImg")
+let counter = 0
+
+const addname = () => {
+  const p = document.getElementById("creador");
+  const name = document.getElementById("nombre");
+  p.innerHTML = `Aplicacion creada por ${name.value}`;
+};
+
+botonEnviar.addEventListener("click", addname);
+
+/////////////////////////////////////////////////////////////
 
 
-boton.addEventListener("click",() => {
-    document.createElement("div");
-    document.classList.add("nuevoD");
-    console.log("sirve")})
+const countImages = () => {
 
-// const createDiv = () =>{
+    const imagenLol = document.getElementById("imagenContainer")
 
-//     document.createElement("div").classList.add("nuevoD")
-//     console.log("sirve")
-// }
+    if (counter === 0) {
+        counter++;
+    } else if (counter >= images.length-1) {
+        counter = 0;
+    } else {
+        counter++;
+    }
 
-// const body = document.getElementsByName("body")[0]
+    console.log(imagenLol.attributes.src.value = images[counter])
 
-console.log(document.getElementsByName("body"))
+}
 
-body.appendChild("click", createDiv())
+botonCambiar.addEventListener("click", countImages)
